160382768cd59785648b0da64370e4e14d87992a
PintTheDragon <PintTheDragon@hotmail.com>
2020-12-30 19:00:32 -0800

    Make broadcasts cleaner, more efficient, and expandable

--- PresetBC/PresetBC/EventHandlers.cs
+++ PresetBC/PresetBC/EventHandlers.cs
@@ -22,73 +22,39 @@ namespace PresetBC
         public void OnCommand(SendingRemoteAdminCommandEventArgs ev)
         {
 
-            Player sender = ev.Sender;
+            var sender = ev.Sender;
 
             if (!sender.CheckPermission("pbc.broadcast"))
             {
                 ev.ReplyMessage = ("Permission Denied.");
-            }
-            else {
-                if (ev.Name == "pre")
-                {
-                    ev.ReplyMessage = ("\n1. " + plugin.Config.ShorthandBroadcast1 + "\n" +
-                    "2. " + plugin.Config.ShorthandBroadcast2 + "\n" +
-                    "3. " + plugin.Config.ShorthandBroadcast3 + "\n" +
-                    "4. " + plugin.Config.ShorthandBroadcast4 + "\n" +
-                    "5. " + plugin.Config.ShorthandBroadcast5 + "\n" +
-                    "6. " + plugin.Config.ShorthandBroadcast6 + "\n" +
-                    "7. " + plugin.Config.ShorthandBroadcast7 + "\n" +
-                    "8. " + plugin.Config.ShorthandBroadcast8 + "\n" +
-                    "9. " + plugin.Config.ShorthandBroadcast9);
-                }
-                else if (ev.Name == "pre1" || ev.Name == "pre" + plugin.Config.ShorthandBroadcast1)
-                {
-                    Map.Broadcast(plugin.Config.BroadcastDuration, plugin.Config.Broadcast1);
-                    ev.ReplyMessage = ("Broadcast Sent.");
-                }
-                else if (ev.Name == "pre2" || ev.Name == "pre" + plugin.Config.ShorthandBroadcast2)
-                {
-                    Map.Broadcast(plugin.Config.BroadcastDuration, plugin.Config.Broadcast2);
-                    ev.ReplyMessage = ("Broadcast Sent.");
-                }
-                else if (ev.Name == "pre3" || ev.Name == "pre" + plugin.Config.ShorthandBroadcast3)
-                {
-                    Map.Broadcast(plugin.Config.BroadcastDuration, plugin.Config.Broadcast3);
-                    ev.ReplyMessage = ("Broadcast Sent.");
-                }
-                else if (ev.Name == "pre4" || ev.Name == "pre" + plugin.Config.ShorthandBroadcast4)
-                {
-                    Map.Broadcast(plugin.Config.BroadcastDuration, plugin.Config.Broadcast4);
-                    ev.ReplyMessage = ("Broadcast Sent.");
-                }
-                else if (ev.Name == "pre5" || ev.Name == "pre" + plugin.Config.ShorthandBroadcast5)
-                {
-                    Map.Broadcast(plugin.Config.BroadcastDuration, plugin.Config.Broadcast5);
-                    ev.ReplyMessage = ("Broadcast Sent.");
-                }
-                else if (ev.Name == "pre6" || ev.Name == "pre" + plugin.Config.ShorthandBroadcast6)
-                {
-                    Map.Broadcast(plugin.Config.BroadcastDuration, plugin.Config.Broadcast6);
-                    ev.ReplyMessage = ("Broadcast Sent.");
-                }
-                else if (ev.Name == "pre7" || ev.Name == "pre" + plugin.Config.ShorthandBroadcast7)
-                {
-                    Map.Broadcast(plugin.Config.BroadcastDuration, plugin.Config.Broadcast7);
-                    ev.ReplyMessage = ("Broadcast Sent.");
-                }
-                else if (ev.Name == "pre8" || ev.Name == "pre" + plugin.Config.ShorthandBroadcast8)
-                {
-                    Map.Broadcast(plugin.Config.BroadcastDuration, plugin.Config.Broadcast8);
-                    ev.ReplyMessage = ("Broadcast Sent.");
-                }
-                else if (ev.Name == "pre9" || ev.Name == "pre" + plugin.Config.ShorthandBroadcast9)
-                {
-                    Map.Broadcast(plugin.Config.BroadcastDuration, plugin.Config.Broadcast9);
-                    ev.ReplyMessage = ("Broadcast Sent.");
-                }
-
+				return;
             }
             
+            if (ev.Name == "pre")
+            {
+                ev.ReplyMessage = "";
+				foreach(var bc in plugin.Config.GetBroadcasts()){
+					ev.ReplyMessage += bc[0] + "\n";
+				}
+            }
+            else if (ev.Name.StartsWith("pre"))
+            {
+				var id = ev.Name.Substring(2);
+				var broadcasts = plugin.Config.GetBroadcasts();
+				
+				if(int.TryParse(id.Trim(), out var index) && index < broadcasts.Count){
+					Map.Broadcast(plugin.Config.BroadcastDuration, broadcasts[index][1]);
+					ev.ReplyMessage = "Broadcast Sent.";
+					return;
+				}
+				
+				var bc = broadcasts.Where(bc => bc[0].Trim().ToLower() == id.Trim().ToLower());
+				if(bc){
+					Map.Broadcast(plugin.Config.BroadcastDuration, bc);
+					ev.ReplyMessage = "Broadcast Sent.";
+					return;
+				}
+            }
         }
     }
 }
